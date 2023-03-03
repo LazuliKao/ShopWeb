501b87c0d816d9704ae426ae96a9fd03ee47e183
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-03 14:32:56 +0800

    sync

--- ShopBackend/src/main/java/com/lazulikao/shop/shopbackend/InentityController.java
+++ ShopBackend/src/main/java/com/lazulikao/shop/shopbackend/InentityController.java
@@ -1,12 +1,15 @@
 package com.lazulikao.shop.shopbackend;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
+@RequestMapping("/identity")
 public class InentityController {
-    @RequestMapping(value = "/identity/login")
-    public String login() {
-        return "success";
+    @GetMapping("/login")
+    @ResponseBody
+    public LoginResult login(String username, String password) {
+        return  new LoginResult(true, "login success" + username);
     }
+    private record LoginResult(boolean success, String message) {}
 }
