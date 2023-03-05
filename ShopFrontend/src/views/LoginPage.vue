9430d4b04793e129acc54b1535d45866cd01edaf
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-05 16:53:04 +0800

    Update LoginPage.vue

--- ShopFrontend/src/views/LoginPage.vue
+++ ShopFrontend/src/views/LoginPage.vue
@@ -1,67 +1,104 @@
-<script lang="ts" >
+<script lang="ts">
 import { TryGetToken, SetToken } from "@/stores/token";
 export default {
-    data() {
-        return {
-            user: "admin",
-            password: "admin",
-            result: "",
-            success: false
+  data() {
+    return {
+      user: "admin",
+      password: "admin",
+      result: "",
+      success: false,
+    };
+  },
+  methods: {
+    Login: async function () {
+      try {
+        let response = await this.axios.post("http://localhost:8080/login", {
+          user: this.user,
+          passwordMd5: this.password,
+        });
+        const {
+          success,
+          message,
+          token,
+        }: {
+          success: boolean;
+          message: string;
+          token: string;
+        } = response.data;
+        this.success = success;
+        if (success) {
+          this.result = message + "\n将在1秒后跳转";
+          setTimeout(() => {
+            this.$router.push("/");
+            SetToken(token);
+          }, 1000);
+        } else {
+          this.result = "登录失败！信息：" + message;
+          ElMessage.error(this.result);
         }
+      } catch (error) {
+        let err = error as Error;
+        this.result = "登录失败！错误：" + err.name + " : " + err.message;
+        ElMessage.error(this.result);
+      }
     },
-    methods: {
-        Submit: async function () {
-            try {
-                let response = await this.axios.post('http://localhost:8080/login', {
-                    user: this.user,
-                    passwordMd5: this.password
-                });
-                const {
-                    success,
-                    message,
-                    token
-                }: {
-                    success: boolean,
-                    message: string,
-                    token: string
-                } = response.data
-                this.success = success;
-                if (success) {
-                    this.result = message + "\n将在1秒后跳转";
-                    setTimeout(() => {
-                        this.$router.push("/")
-                        SetToken(token)
-                    }, 1000);
-                } else {
-                    this.result = "登录失败！信息：" + message;
-                    ElMessage.error(this.result);
-                }
-            } catch (error) {
-                let err = (error as Error);
-                this.result = "登录失败！错误：" + err.name + " : " + err.message;
-                ElMessage.error(this.result);
-            }
-        }
-    }
-}
+  },
+};
 </script>
 <template>
-    <div class="main">
-        帐号：<el-input v-model="user" placeholder="输入帐号" clearable />
-        <br>
-        密码：<el-input type="password" placeholder="输入密码" v-model="password" show-password clearable />
-        <br>
-        <h4>
-            {{ result }}
-            <a v-if="success" href="#/">主页</a>
-        </h4>
-        <el-button @click="Submit" class="loginbutton">
-            登录
-        </el-button>
-        <el-button @click="Submit" class="loginbutton">
-            <RouterLink to="/register">前往注册</RouterLink>
-        </el-button>    
+  <div class="main">
+    <div class="input-container">
+      <label>帐号：</label>
+      <el-input v-model="user" placeholder="输入帐号" clearable />
     </div>
+    <br />
+    <div class="input-container">
+      <label>密码：</label>
+      <el-input
+        type="password"
+        placeholder="输入密码"
+        v-model="password"
+        show-password
+        clearable
+      />
+    </div>
+    <br />
+    <div class="button-container">
+      <el-button @click="Login" type="primary"> 登录 </el-button>
+      <RouterLink to="/register"> <el-button>前往注册 </el-button></RouterLink>
+    </div>
+    <h4>
+      {{ result }}
+      <a v-if="success" href="#/">主页</a>
+    </h4>
+  </div>
 </template>
+
 <style scoped>
+.el-input {
+  width: 200px;
+}
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.input-container {
+  display: flex;
+}
+
+.input-container label {
+  width: 60px;
+  display: flex;
+  align-items: center;
+}
+.button-container {
+  display: flex;
+  justify-content: space-between;
+  width: 220px;
+}
+.el-button {
+  width: 100px;
+}
 </style>
