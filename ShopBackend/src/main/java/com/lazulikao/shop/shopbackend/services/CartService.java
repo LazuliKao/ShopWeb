9b3b225cba46f3452e8fe78a9f7a020b9083c331
LazuliKao <lazulikao@nbt.edu.cn>
2023-03-27 18:08:41 +0800

    Update CartService.java

--- ShopBackend/src/main/java/com/lazulikao/shop/shopbackend/services/CartService.java
+++ ShopBackend/src/main/java/com/lazulikao/shop/shopbackend/services/CartService.java
@@ -1,10 +1,6 @@
 package com.lazulikao.shop.shopbackend.services;
 import com.lazulikao.shop.shopbackend.entity.CartItem;
-import com.lazulikao.shop.shopbackend.entity.ShopItem;
 import com.lazulikao.shop.shopbackend.repository.CartRepository;
-import com.lazulikao.shop.shopbackend.repository.ShopRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
