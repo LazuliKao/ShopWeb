a8342cf56de87071cc5413e4965f90892b1054c6
LazuliKao <lazulikao@nbt.edu.cn>
2023-04-17 18:24:12 +0800

    sync

--- ShopBackend/src/main/java/com/lazulikao/shop/shopbackend/config/CrosConfig.java
+++ ShopBackend/src/main/java/com/lazulikao/shop/shopbackend/config/CrosConfig.java
@@ -22,7 +22,10 @@ public class CrosConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173");
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
 
